6ceb663e11dfcb1fc5c03e6e5d95cb448c8a60bc
liuxianfeng521 <735801190@qq.com>
2019-02-14 14:20:52 +0800

    修改logoutmock数据

--- src/mock/login/index.js
+++ src/mock/login/index.js
@@ -1,5 +1,5 @@
 import Mock from 'mockjs'
 
 Mock.mock(/\/user\/login/, /post/i, { 'code': 20000, 'data': { 'token': 'admin' }})
-Mock.mock(/\/user\/loginout/, /post/i, { 'code': 20000, 'data': { 'token': 'admin' }})
+Mock.mock(/\/user\/logout/, /post/i, { 'code': 20000, 'data': { 'token': 'admin' }})
 Mock.mock(/\/user\/info/, /get/i, { 'code': 20000, 'data': { 'roles': ['admin'], 'name': 'admin', 'avatar': '' }})
