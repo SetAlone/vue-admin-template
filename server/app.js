bb02321f7ebefbc110c83ff172a1dc3831e5146f
liuxianfeng521 <735801190@qq.com>
2019-02-28 16:30:14 +0800

    新增express后端功能

---
+++ server/app.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var fs = require('fs')
+var path = require('path')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+// 注册users接口
+var users = require('./routes/users')
+app.use('/users', users)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.get('/about', function(req, res) {
+  console.log('---', req, res)
+  res.send('about')
+})
+
+// 访问静态资源
+app.use(express.static(path.resolve(__dirname, '../dist')))
+
+// 访问单页
+app.get('*', function(req, res) {
+  var html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf-8')
+  res.send(html)
+})
+
+// 监听
+app.listen(8889, function() {
+  console.log('success listen...8889')
+})
